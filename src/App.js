5bec30727678ea917ebb4ce4a5275a34a022fb84
asimboga <asimboga@gmail.com>
2022-07-05 21:03:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Button, { TomatoButton } from './components/Button.styled';
+import Container from './components/Container.styled';
+import HeaderText from './components/Header.styled';
+import StyledLink from './components/Link.styled';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container bg="green">
+    <HeaderText>STYLED COMPONENTS</HeaderText>
+      <Button primary>Click</Button>
+      <Button>Click</Button>
+      <TomatoButton>Click</TomatoButton>
+      <TomatoButton primary>Click</TomatoButton>
+      <HeaderText color="white">STYLED COMPONENTS</HeaderText>
+      <StyledLink href='https://clarusway.com' target="_blank">Clarusway</StyledLink>
+
+    </Container>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Link.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+font-size: 1.5rem;
+margin: 1rem 0;
+text-decoration: none;
+`;
+
+export default StyledLink;
